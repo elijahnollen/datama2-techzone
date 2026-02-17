9c70e2a824b27451f2ad5fe9d54857aadccb1072
AcheDiamond <acchavez@student.apc.edu.ph>
2026-02-18 07:56:29 +0800

    debug

--- server/config/app.php
+++ server/config/app.php
@@ -1,6 +1,10 @@
 <?php
 // server/config/app.php
-// Prototype constants (until real login replaces them)
 
+// IMPORTANT: set this to your XAMPP folder name.
+// Since i my URL works at http://localhost/auth/..., BASE_URL should be "/auth".
+define('BASE_URL', '/auth');
+
+// Temporary prototype IDs until full login replaces them
 define('PROTOTYPE_CUSTOMER_ID', 1);
 define('PROTOTYPE_EMPLOYEE_ID', 1);
