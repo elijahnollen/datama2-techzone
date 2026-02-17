9fa640e8c2b6c95421cc905d220dca1f35f79ada
AcheDiamond <acchavez@student.apc.edu.ph>
2026-02-18 07:28:14 +0800

    added the db and session

---
+++ server/config/database.php
@@ -0,0 +1,17 @@
+<?php
+function db(): PDO {
+    static $pdo = null;
+    if ($pdo) return $pdo;
+
+    $pdo = new PDO(
+        "mysql:host=localhost;dbname=techzone_new_inventory;charset=utf8mb4",
+        "root",
+        "",
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+
+    return $pdo;
+}
